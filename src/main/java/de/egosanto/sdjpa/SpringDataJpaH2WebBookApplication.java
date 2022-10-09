ed8c6e4bbf57811e19895205f352365b2dbbeb45
Toni Zeidler <toni.z@gmx.de>
2022-10-09 09:49:23 +0200

    Start Project

---
+++ src/main/java/de/egosanto/sdjpa/SpringDataJpaH2WebBookApplication.java
@@ -0,0 +1,13 @@
+package de.egosanto.sdjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaH2WebBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaH2WebBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/egosanto/sdjpa/SpringDataJpaH2WebBookApplicationTests.java
@@ -0,0 +1,13 @@
+package de.egosanto.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaH2WebBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
